65756279b8fd3377fa6be8c22543d10c173e7c7a
Eleanor Lee <ellie007@gmail.com>
2013-12-11 17:00:03 -0600

    page 122

--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -16,23 +16,18 @@ module Codebreaker
     end
 
     def total_match_count
-      count = 0
       secret = @secret.split('')
-      @guess.split('').map do |n|
-        if secret.include?(n)
-          secret.delete_at(secret.index(n))
-          count += 1
-        end
+      @guess.split('').inject(0) do |count, n|
+        count + (delete_first(secret, n) ? 1 : 0)
       end
-      count
+    end
+
+    def delete_first(code, n)
+      code.delete_at(code.index(n)) if code.index(n)
     end
 
     def exact_match?(index)
       @guess[index] == @secret[index]
-    end
-
-    def number_match?(index)
-      @secret.include?(@guess[index]) && !exact_match?(index)
     end
 
   end
